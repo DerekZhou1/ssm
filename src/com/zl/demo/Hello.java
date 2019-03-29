c1a515c1709f7652d0af926815c4f653a2f41990
Administrator <284280144@qq.com>
2019-03-29 16:15:20 +0800

    add ignore,add main function

---
+++ src/com/zl/demo/Hello.java
@@ -0,0 +1,10 @@
+package com.zl.demo;
+
+/**
+ * Created by Administrator on 2019/3/29 0029.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
